011898f00fec6c90dd5f7690a0c124e8a9550d43
Marion E Smith <titleofliberty@users.noreply.github.com>
2023-06-08 19:28:36 -0400

    backup

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const jetpack = require("fs-jetpack");
-const moment = require("moment/moment");
 var win = nw.Window.get();
 
 
 function togglePanel() {
-    var pnl = document.getElementById("panel");
-    pnl.classList.toggle("pnl-hidden");
-    console.log("toggle");
+    var pnl = $("#panel");
+    console.log(pnl);
 }
+
+function calDates() {
+    var today = new Date().toISOString();
+    var dates = $(".cal-date");
+    dates.each(function (index) {
+        var date = $(this).attr("data-date");
+        if (date.slice(5, 7) != today.slice(5, 7)) {
+            $(this).addClass("text-secondary");
+        }
+        else {
+            $(this).removeClass("text-secondary");
+            if (date.slice(8, 10) == today.slice(8, 10)) {
+                $(this).addClass("fw-bold");
+                $(this).addClass("border");
+                $(this).addClass("border-2");
+            }
+            else {
+                $(this).removeClass("fw-bold");
+                $(this).removeClass("border");
+                $(this).removeClass("border-2");
+            }
+        }        
+    });
+}
+
+calDates();
 
 win.setPosition("center");
