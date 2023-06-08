e6e6de04b5b5cee3787af6d60eef87e59ec653fc
Marion E Smith <titleofliberty@users.noreply.github.com>
2023-06-08 08:10:49 -0400

    backup

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ const moment = require("moment/moment");
 var win = nw.Window.get();
 
 
+function togglePanel() {
+    var pnl = document.getElementById("panel");
+    pnl.classList.toggle("pnl-hidden");
+    console.log("toggle");
+}
+
 win.setPosition("center");
