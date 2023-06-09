79c836f62bf70ff4ad630cf885eed4e8a3f16923
Marion E Smith <titleofliberty@users.noreply.github.com>
2023-06-09 15:53:00 -0400

    backup

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const jetpack = require("fs-jetpack");
+const dayjs = require('dayjs');
 
 var win = nw.Window.get();
 var selectedDate = new Date();
@@ -11,28 +12,21 @@ function togglePanel() {
 
 function calPopulate(date) {
     selectedDate = date;
-    var step = selectedDate;
+    var step = selectedDate.startOf("month").startOf("week");
     var calDates = $(".cal-date");
 
-    step.setDate(1);
-    while (step.getDay() != 0) {
-        step.setDate(step.getDate() - 1);
-    }
-    $("#cal-month").text(months[selectedDate.getMonth()]);
-    $("#cal-year").text(selectedDate.getFullYear());
+    $("#cal-month").text(months[selectedDate.month()]);
+    $("#cal-year").text(selectedDate.year());
     calDates.each(function (index) {
-        var y = step.getFullYear();
-        var m = step.getMonth();
-        var d = step.getDate();
-        $(this).data("date", y + "-" + m + "-" + d);
-        $(this).text(d);
-        console.log(index, selectedDate, step);
-        if (m != selectedDate.getMonth()) {
+        $(this).data("date", step.format("YYYY-MM-DD"));
+        $(this).text(step.date());
+        console.log(step.month(), selectedDate.month());
+        if (step.month() != selectedDate.month()) {
             $(this).addClass("text-secondary");
         }
         else {
             $(this).removeClass("text-secondary");
-            if (d == selectedDate.getDate()) {
+            if (step.date() == selectedDate.date()) {
                 $(this).addClass("fw-bold");
                 $(this).addClass("border");
                 $(this).addClass("border-2");
@@ -43,10 +37,24 @@ function calPopulate(date) {
                 $(this).removeClass("border-2");
             }
         }   
-        step.setDate(step.getDate() + 1);
+        step = step.add(1, "day");
+        console.log(step);
     });
 }
 
-calPopulate(new Date(2021, 0, 1));
+function calToday() {
+    calPopulate(dayjs());
+}
+
+function calGoTo(date) {
+    console.log("calGoTo", date);
+    calPopulate(dayjs(date));
+}
+
+$(".cal-date").on("click", function () {
+    calGoTo($(this).data("date"));
+});
+
+calPopulate(dayjs("2021-01-01"));
 
 win.setPosition("center");
