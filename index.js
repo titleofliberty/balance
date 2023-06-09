5c645f0c2eea8d892af7df8dde37171aee382d44
Marion E Smith <titleofliberty@users.noreply.github.com>
2023-06-08 21:36:26 -0400

    backup

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 const jetpack = require("fs-jetpack");
-var win = nw.Window.get();
 
+var win = nw.Window.get();
+var selectedDate = new Date();
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 function togglePanel() {
     var pnl = $("#panel");
     console.log(pnl);
 }
 
-function calDates() {
-    var today = new Date().toISOString();
-    var dates = $(".cal-date");
-    dates.each(function (index) {
-        var date = $(this).attr("data-date");
-        if (date.slice(5, 7) != today.slice(5, 7)) {
+function calPopulate(date) {
+    selectedDate = date;
+    var step = selectedDate;
+    var calDates = $(".cal-date");
+
+    step.setDate(1);
+    while (step.getDay() != 0) {
+        step.setDate(step.getDate() - 1);
+    }
+    $("#cal-month").text(months[selectedDate.getMonth()]);
+    $("#cal-year").text(selectedDate.getFullYear());
+    calDates.each(function (index) {
+        var y = step.getFullYear();
+        var m = step.getMonth();
+        var d = step.getDate();
+        $(this).data("date", y + "-" + m + "-" + d);
+        $(this).text(d);
+        console.log(index, selectedDate, step);
+        if (m != selectedDate.getMonth()) {
             $(this).addClass("text-secondary");
         }
         else {
             $(this).removeClass("text-secondary");
-            if (date.slice(8, 10) == today.slice(8, 10)) {
+            if (d == selectedDate.getDate()) {
                 $(this).addClass("fw-bold");
                 $(this).addClass("border");
                 $(this).addClass("border-2");
@@ -27,10 +42,11 @@ function calDates() {
                 $(this).removeClass("border");
                 $(this).removeClass("border-2");
             }
-        }        
+        }   
+        step.setDate(step.getDate() + 1);
     });
 }
 
-calDates();
+calPopulate(new Date(2021, 0, 1));
 
 win.setPosition("center");
